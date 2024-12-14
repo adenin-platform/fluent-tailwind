949c51184fa88eaee39a5250cd2fa4edf1d2f527
Martin Amm <IntranetFactory@users.noreply.github.com>
2024-12-14 19:15:05 +0000

    configured shadcn and added Button

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path"
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
@@ -16,5 +17,10 @@ export default defineConfig({
   },
   build: {
     sourcemap: true
-  }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
